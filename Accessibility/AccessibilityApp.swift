59819f42f28faad75626b98695aacb377f9050eb
Olivier Van hamme <nicefiction@gmail.com>
2021-08-18 10:40:24 +0200

    Initial Commit

---
+++ Accessibility/AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityApp.swift
+//  Accessibility
+//
+//  Created by Olivier Van hamme on 18/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
